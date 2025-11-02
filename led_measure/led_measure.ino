ae4b149008d92bee3925bb081e4c312f554101c1
wjddn0719 <jungwoo07191@naver.com>
2025-11-02 14:52:35 +0000

    first commit

--- led_measure/led_measure.ino
+++ led_measure/led_measure.ino
@@ -1,23 +1,30 @@
-#define REF_RESISTOR 10000.0
-#define LED 7
-#define SUPPLY A0                              
-void setup(){
+#define STANDARD_RESISTOR 300.0
+#define SUPPLY 5.0
+
+void setup() {
     Serial.begin(9600);
-    pinMode(LED, OUTPUT);    
 }
 
-void loop(){
-    int adcValue = analogRead(0);
-    float vMeasured = adcValue * (5.0/1023.0);
-    float vSupply = 5.0;
-    float rUnknown = REF_RESISTOR * (vMeasured/(vSupply - vMeasured));
-    Serial.print("ADC : ");
-    Serial.print(adcValue);
-    Serial.print("\tV_measured: ");
-    Serial.print(vMeasured,3);
-    Serial.print("V\tR_unknown: ");
-    Serial.print(rUnknown, 2);
-    Serial.println(" ohm");
-    digitalWrite(LED, HIGH);
-    delay(500);
+void loop() {
+    int digital_A1 = analogRead(A1);
+    int digital_A0 = analogRead(A0);
+
+    float analog_A1 = digital_A1 * (5.0/1023.0);
+    float analog_A0 = digital_A0 * (5.0 / 1023.0);
+    
+    float voltage_descent = SUPPLY - analog_A1;
+    float current = voltage_descent / STANDARD_RESISTOR;
+    
+    float unknown_voltage = analog_A1 - analog_A0;
+    float unknown_resistor = unknown_voltage/current;
+
+    Serial.print("led_name=");
+    Serial.print("RED");
+    Serial.print(", supply_voltage=");
+    Serial.print(SUPPLY, 2);
+    Serial.print(", led_voltage=");
+    Serial.print(analog_A0, 3);
+    Serial.print(", led_current=");
+    Serial.println(current,6);
+    delay(1000);
 }
